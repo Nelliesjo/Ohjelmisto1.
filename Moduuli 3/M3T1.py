08e1aad0c01a629fee584262d43a7d13bcd426b5
nelliesjo <nellies@metropolia.fi>
2025-01-21 14:16:39 +0200

    g

---
+++ Moduuli 3/M3T1.py
@@ -0,0 +1,21 @@
+kuha = float(input("Mikä on kuhan pituus senttimetreinä?: "))
+minpituus =int(37)
+
+cm = (minpituus - kuha)
+
+if kuha >= 37:
+    print("Voit pitää kuhan.")
+
+if kuha <= 36.9:
+    print(f"Kuha on {cm}cm liian pieni, laske kuha takasisin järveen.")
+
+kuha = float(input("Mikä on kuhan pituus senttimetreinä?: "))
+minpituus =(37)
+
+cm = (minpituus - kuha)
+
+if kuha >= 37:
+    print("Voit pitää kuhan.")
+
+else:
+    print(f"Kuha on {cm}cm liian pieni, laske kuha takasisin järveen.")
