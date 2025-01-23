d9bc05751b985bba9e703b29355e56f9b3de75dd
nelliesjo <nellies@metropolia.fi>
2025-01-23 13:40:04 +0200

    Tehtävä 4

--- Moduuli 2/M2T6.py
+++ Moduuli 2/M2T6.py
@@ -5,3 +5,15 @@ print(satunnaisluku)
 
 satunnaisluku2=random.sample([1,2,3,4,5,6],4)
 print(satunnaisluku2)
+
+A=(random.randint(1,9))
+B=(random.randint(1,9))
+C=(random.randint(1,9))
+print(f"[{A}, {B}, {C}]")
+
+A1=(random.randint(1,6))
+B1=(random.randint(1,6))
+C1=(random.randint(1,6))
+D1=(random.randint(1,6))
+
+print(f"[{A1}, {B1}, {C1}, {D1}]")
