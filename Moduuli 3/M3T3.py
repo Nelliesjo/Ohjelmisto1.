4cd685bed2f51d05aa68a958f05c24113b8d42aa
nelliesjo <nellies@metropolia.fi>
2025-01-23 10:30:00 +0200

    Tehtävä 3

--- Moduuli 3/M3T3.py
+++ Moduuli 3/M3T3.py
@@ -1,10 +1,18 @@
 sukupuoli = input("Onko biologinen sukupuolesi nainen vai mies?:")
 hgarvo = float(input("Mikä on sinun hemoglobiini arvosi?:"))
 
-if sukupuoli == 'nainen' and hgarvo 117 <= hgarvo < 175:
-    print("hemoglobiiniarvo on normaali.")
+if sukupuoli == 'nainen':
+    if 117 <= hgarvo < 175:
+        print("Hemoglobiiniarvo on normaali.")
+    elif 117 > hgarvo:
+        print("Hemoglobiini rvo on alhainen.")
+    else:
+        print("Hemoglobiiniarvo on korkea.")
 
-
-
-#Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
-#Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+if sukupuoli == 'mies':
+    if 134 <= hgarvo < 195:
+        print("hemoglobiiniarvo on normaali.")
+    elif 143 > hgarvo:
+        print("Hemoglobiini rvo on alhainen.")
+    else:
+        print("Hemoglobiiniarvo on korkea.")
