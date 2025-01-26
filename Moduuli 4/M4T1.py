c7d5180338ad39f96bd467dc22b1352b36a46f18
nelliesjo <nellies@metropolia.fi>
2025-01-26 20:40:54 +0200

    Moduulin 4 tehtävät

--- Moduuli 4/M4T1.py
+++ Moduuli 4/M4T1.py
@@ -6,17 +6,3 @@ while luku <= 1000:
 
 print("done")
 
-
-
-
-#xx
-
-luku = 1
-#yhteensä tai pienempi kuin 30, ei listaa siitä pidemnmälle.
-while luku <= 30:
-    #onko luku jaollinen kolmella?
-    if luku % 3 == 0:
-        print(luku)
-    luku = luku + 1  # # luku += 1 :sama asia kuin: luku = luku+1
-
-print("done")
