7ccfdcd4882c5a01fbc40d7a968294c0e20614b3
nelliesjo <nellies@metropolia.fi>
2025-02-04 13:51:34 +0200

    Moduulin 5 tehtävät

--- Moduuli 5/M5T2.py
+++ Moduuli 5/M5T2.py
@@ -1,9 +1,3 @@
-#Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
-# kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi. Lopuksi
-# ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä
-# suurimmasta alkaen. Vihje: listan alkioiden lajittelujärjestyksen voi kääntää
-# antamalla sort-metodille argumentiksi reverse=True.
-
 luvut =[]
 
 luku = input("Anna luku, tyjhä lopettaa:")
@@ -14,13 +8,3 @@ while luku != "":
 
 luvut.sort(reverse=True)
 print(luvut [0:5])
-
-#tulos = luvut[0:6]
-#print (tulos)
-
-
-
-
-
-#while
-#for
